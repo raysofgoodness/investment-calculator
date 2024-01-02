0c92a186e1db324025dcaf395d463909f16d6635
raysofgoodness <shady5324@gmail.com>
2024-01-02 13:05:45 +0200

    calculateInvestmentResults data

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         setUserInput(prevState => {
             return {
                 ...prevState,
-                [inputIdentificator]: inputValue,
+                [inputIdentificator]: +inputValue,
             }
         });
     }
--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,6 +1,9 @@
+import { calculateInvestmentResults } from '../../util/investment';
 import './Results.css';
 
 function Results({ userInput }) {
+    const resultsData = calculateInvestmentResults(userInput);
+
     return <p>Results...</p>
 }
 
